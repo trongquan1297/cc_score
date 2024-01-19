47c3e1c7ca431864c6d0f8e107670f8f4d23e214
quann <quannt@appotapay.com>
2024-01-19 17:07:10 +0700

    fix: up server

--- main.go
+++ main.go
@@ -9,26 +9,6 @@ import (
 	"net/http"
 )
 
-func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Thiết lập các headers cho CORS
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		// Cho phép trình duyệt gửi các headers sau khi nhận một phản hồi từ server
-		w.Header().Set("Access-Control-Expose-Headers", "Content-Type")
-
-		// Nếu đây là một yêu cầu OPTIONS (pre-flight), không xử lý tiếp và trả về ngay
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		// Gọi handler tiếp theo trong chuỗi middleware
-		next(w, r)
-	}
-}
-
 func main() {
 	cfg, err := config.LoadConfig("config.yml")
 	if err != nil {
@@ -69,7 +49,7 @@ func main() {
 
 	http.HandleFunc("/addScore", scoreboard.PostAddScoreHandler(scoreBoard))
 	http.HandleFunc("/getHighestScore", scoreboard.GetHighestScoreHandler(scoreBoard))
-	http.HandleFunc("/getTop5Players", corsMiddleware(scoreBoard.GetTopPlayersHandler()))
+	http.HandleFunc("/getTop5Players", scoreBoard.GetTopPlayersHandler())
 	fmt.Println("Server is running on :8080")
 	http.ListenAndServe(":8080", nil)
 }
